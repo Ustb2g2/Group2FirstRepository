4955001122d005ca89718482a119c5fc49c8886a
dev29 <dev29@dev29-DL-960GM-GS3-FX>
2019-09-24 14:53:09 +0530

    initial commit by simi

---
+++ SalesToolV2019/src/com/ust/test/Main.java
@@ -0,0 +1,18 @@
+package com.ust.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    SalesData data = new SalesData();
+    System.out.println("Welcome to GitHub!!!");
+    displayGreeting();
+    data.display();
+    
+	}
+	private static void displayGreeting() {
+		System.out.println("Welcome to Sales app");
+		
+	}
+
+}
---
+++ SalesToolV2019/src/com/ust/test/SalesData.java
@@ -0,0 +1,8 @@
+package com.ust.test;
+
+public class SalesData {
+	public void display() {
+		System.out.println("Welcome to Main");
+	}
+
+}
